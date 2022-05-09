418a7dc1659f94a43296fc03c29233fa53812bd9
kristoferfannar <kristoferfannarb@gmail.com>
2022-05-09 01:48:24 +0000

    finished

--- planet.py
+++ planet.py
@@ -4,7 +4,7 @@ import math
 
 class Planet:
     def __init__(self, distance, color, mass, radius) -> None:
-        self.distance = distance
+        self.distance = abs(distance)
         self.x = distance * DISTANCESCALE + WIDTH / 2 
         self.y = HEIGHT / 2
         self.color = color
@@ -17,8 +17,8 @@ class Planet:
 
         if distance != 0:
             self.sun = False
-            self.radius *= 50
-            self.y_vel = math.sqrt(G*SUNMASS/self.distance) #* DISTANCESCALE
+            self.radius *= 100
+            self.y_vel = math.sqrt(G*SUNMASS/self.distance)
 
 
 
@@ -26,17 +26,18 @@ class Planet:
     def calc_vel(self):
         if self.sun: return
 
-        x_distance = (self.x - (WIDTH / 2))
-        y_distance = (self.y - (HEIGHT / 2))
+        x_distance = WIDTH / 2 - self.x
+        y_distance = HEIGHT / 2 - self.y
         distance = math.sqrt(x_distance**2 + y_distance**2)
+
         force = G*SUNMASS * self.mass / (distance**2)
         theta = math.atan2(y_distance, x_distance)
         x_force = math.cos(theta) * force
         y_force = math.sin(theta) * force
 
 
-        self.x_vel += x_force / SUNMASS * TIMESTEP
-        self.y_vel += y_force / SUNMASS * TIMESTEP
+        self.x_vel += x_force / self.mass * TIMESTEP * DISTANCESCALE#tok mig allan dag að fatta að hafa * DISTANCESCALE. 
+        self.y_vel += y_force / self.mass * TIMESTEP * DISTANCESCALE #var að klara nuna kl 1:20
 
         #print(x_force, y_force)
         #print(x_distance, y_distance)
@@ -53,17 +54,18 @@ class Planet:
 
     
     def draw(self):
+        self.draw_orbit()
         pygame.draw.circle(WINDOW, self.color, (self.x, self.y), self.radius * RADIUSSCALE)
         self.update()
-        self.draw_orbit()
 
     
     def draw_orbit(self):
         for coords in self.orbit:
-            pygame.draw.circle(WINDOW, self.color, coords, 2)
+            pygame.draw.circle(WINDOW, WHITE, coords, 1)
 
 
 
 
 if __name__ == "__main__":
-    print(math.sqrt(G*SUNMASS/AU))
+    print (AU / (1.496e11) )
+    print ( 6.67e-11 / G)
--- solarsystem.py
+++ solarsystem.py
@@ -14,9 +14,9 @@ def main():
     
     sun = Planet(0, YELLOW, SUNMASS, SUNRADIUS)
     mercury = Planet(0.4*AU, MERCURY, MERCURYMASS, MERCURYRADIUS)
-    venus = Planet(0.7*AU, VENUS, VENUSMASS, VENUSRADIUS)
+    venus = Planet(-0.7*AU, VENUS, VENUSMASS, VENUSRADIUS)
     earth = Planet(AU, EARTH, EARTHMASS, EARTHRADIUS)
-    mars = Planet(1.5*AU, MARS, MARSMASS, MARSRADIUS)
+    mars = Planet(-1.5*AU, MARS, MARSMASS, MARSRADIUS)
     jupiter = Planet(5.2*AU, JUPITER, JUPITERMASS, JUPITERRADIUS*0.1)
 
     planets = [sun, earth, mars, venus, mercury, jupiter]
